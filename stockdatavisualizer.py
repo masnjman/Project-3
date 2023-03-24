6902565a8fbe4659aa0a400396977642516d70f7
Ethan Koen <epkknd@umsystem.edu>
2023-03-24 11:14:24 -0500

    created menu

--- stockdatavisualizer.py
+++ stockdatavisualizer.py
@@ -1,16 +1,27 @@
 import requests
+import json
 
-def av_query(symbol, function):
+def AV_Query(symbol, function):
     apikey = "T6IGSBW0GEMTVTMA"
     url = 'https://www.alphavantage.co/query?function=' + function + '&symbol=' + symbol + '&apikey=' + apikey
     r = requests.get(url)
     data = r.json()
     return data
-    
+
+def menu():
+    stocksymbol = input("Enter the stock symbol: ")
+    charttype = input("Enter the chart type: ")
+    function = input("Enter the time series function: ")
+    startdate = input("Enter the beginning date in YYYY-MM-DD format: ")
+    enddate = input("Enter the end date in YYYY-MM-DD format: ")
+        
+   
+   
     
 def main():
-    data = av_query("IBM", "TIME_SERIES_MONTHLY_ADJUSTED")
+    menu()
+    data = AV_Query("I", "TIME_SERIES_MONTHLY_ADJUSTED")
     print(data)
-    
+  
 main()
     
